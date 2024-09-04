ddf8c3acfd2c14f91f08f9fef0be37557a317b76
ksivasundari <104321809+ksivasundari@users.noreply.github.com>
2024-09-04 18:19:04 -0400

    Add files via upload

---
+++ frmActiveWin.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmActiveWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(163, 438);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(189, 78);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(454, 438);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(183, 77);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbText.Font = new System.Drawing.Font("Calibri", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbText.Location = new System.Drawing.Point(238, 72);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtbText.Size = new System.Drawing.Size(278, 320);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = "00";
+            // 
+            // frmActiveWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 589);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmActiveWin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ActiveWin";
+            this.Load += new System.EventHandler(this.frmActiveWin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox rtbText;
+    }
+}
+
---
+++ frmActiveWin.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmActiveWin : Form
+    {
+        public frmActiveWin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmActiveWin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Move the mouse 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //Simulate left mouse button press 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //Simulate the left mouse button up 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //Simulate the right mouse button press 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //Simulate the right mouse button up 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //Simulate the middle mouse button press 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //Simulate the middle mouse button up 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //Mark whether to use absolute coordinates 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        //Simulate the scrolling operation of the mouse wheel, must cooperate with the dwData parameter
+        const int MOUSEEVENTF_WHEEL = 0x0800;
+        static uint DOWN = 0x0002;
+        static uint UP = 0x0004;
+        //static uint MOUSEEVENTF_ABSOLUTE = 0x8000;
+        //static uint MOUSEEVENTF_MOVE = 0x0001;
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        static void MouseMove()
+        {
+            Thread.Sleep(100);
+            if ((Count % 2) == 0)
+            {
+                //mouse_event(DOWN | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, 800, 800, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }
+            else
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //Thread.Sleep(100);
+                //mouse_event(UP | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, 800, 800, 0, 0);
+            }
+        }
+
+        static int Count = 0;
+        static int intervalTime = 60000;
+        static int intervalmin = 2;
+        private int waitTime = intervalTime * intervalmin;
+        private DateTime startTime;
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // t = new Timer { Interval = 1000 };
+            rtbText.Text = String.Format("0{0}", intervalmin);
+            timer.Interval = intervalTime;
+            timer.Tick += timer_Tick;
+            timer.Start(); // start timer (you can do it on form load, if you need)
+            startTime = DateTime.Now; // and remember start time
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int elapsedTime = (int)(DateTime.Now - startTime).TotalMinutes;
+            int remainingTime = intervalmin - elapsedTime;
+            string pre = string.Empty;
+            if (remainingTime <= 0)
+            {
+                Count++;
+                // run your function
+                //timer.Stop();
+                MouseMove();
+                startTime = DateTime.Now;
+                remainingTime = intervalmin;
+            }
+            if (remainingTime < 10)
+            {
+                pre = "0";
+            }
+
+            rtbText.Text = String.Format("{0}",(pre + remainingTime));
+               // String.Format("{0} seconds remaining...", remainingSeconds);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+       
+    }
+}
